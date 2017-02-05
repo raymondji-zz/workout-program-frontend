98bfc5ddfdadb865c99f29e97a75387222ebad40
Raymond Ji <raymondji.me@gmail.com>
2017-02-05 12:57:48 -0500

    make programBuilderPage a class

--- src/containers/ProgramBuilderPage.js
+++ src/containers/ProgramBuilderPage.js
@@ -2,26 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-// export class ProgramBuilderPage extends Component {
-//     componentWillMount() {
-//         console.log("HI");
-//     }
+export class ProgramBuilderPage extends Component {
+    componentWillMount() {
+        console.log("HI");
+    }
 
-//     render() {
-//         return (
-//             <div>
-//                 Hello World!
-//             </div>
-//         );
-//     }
-// }
-
-const ProgramBuilderPage = () => {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
-};
-
-export default ProgramBuilderPage;
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
